f2ab1e5f3922b3a9422cf1787b10b71b14d2b272
Daniel Vianna <dmlvianna@gmail.com>
2018-11-10 20:03:18 +1100

    Exercise 04 solved

--- code/exercises/src/Ex04/Exercise.hs
+++ code/exercises/src/Ex04/Exercise.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE CPP #-}
 module Ex04.Exercise where
 
-import qualified Data.Map as Map
+import qualified Data.Map    as Map
+import qualified Data.Text   as Text
 
-import Reflex
+import           Reflex
 
 #ifndef ghcjs_HOST_OS
-import Util.Run
+import           Util.Run
 #endif
 
-import Ex04.Common
-import Ex04.Run
+import           Ex04.Common
+import           Ex04.Run
 
 ex04 ::
   Reflex t =>
@@ -18,14 +19,40 @@ ex04 ::
   Outputs t
 ex04 (Inputs bMoney bCarrot bCelery bCucumber bSelected eBuy eRefund) =
   let
+    name :: Stock -> Text.Text
+    name s = pName . sProduct $ s
+    -- lbStock :: [Behavior t Stock]
+    lbStock =
+      [bCarrot, bCelery, bCucumber]
+    bName :: Reflex t
+          => Behavior t Stock
+          -> Behavior t (Text.Text, Stock)
+    bName bs = (,) <$> (fmap name bs) <*> bs
+    -- ltStock :: Behavior t (Map.Map Text.Text Stock)
+    bmStock = Map.fromList <$> (sequence $ bName <$> lbStock)
+    -- eStock :: Event t Stock
+    eStock =
+      attachWithMaybe Map.lookup bSelected (bmStock <@ eBuy)
+    checkNotEnoughStock s =
+      sQuantity s == 0
+    checkNotEnoughMoney money p =
+      money < pCost p
+    eNotEnoughMoney =
+      NotEnoughMoney <$ ffilter id
+      (checkNotEnoughMoney <$> bMoney <@> (sProduct <$> eStock))
+    eItemOutOfStock =
+      ItemOutOfStock <$ ffilter id
+      (checkNotEnoughStock <$> eStock)
+    eSale =
+      difference eStock eError
     eVend =
-      never
+      pName . sProduct <$> eSale
     eSpend =
-      never
+      pCost . sProduct <$> eSale
     eChange =
-      never
+      bMoney <@ eRefund
     eError =
-      never
+      leftmost [eItemOutOfStock, eNotEnoughMoney]
   in
     Outputs eVend eSpend eChange eError
 
